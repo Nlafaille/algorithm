912a3e72030287bc647d5a47c95680424a43c61c
Nlafaille <lafaille@outlook.com>
2025-01-06 22:04:42 +1000

    Add Problem1.py and Revision1.py with filtering logic

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,16 @@
-source_file_name = input("Source file name: ")
+source_file_name = input(f"Source file name: ")
+target_file_name = input(f"Target file name: ")
 
-with open(source_file_name, 'r') as source_file:
-    source_file_contents = source_file.read()
+removed_lines_count = 0 
+
+with open(source_file_name, 'r') as source_file, open(target_file_name, 'w') as target_file: 
+
+    for line in source_file:
+        #strip() function means non empty
+        if line.strip(): 
+            target_file.write(line)
+        else:
+            #Increment the counter for removed lines
+            removed_lines_count  += 1
+print(f"Lines removed: {removed_lines_count}")
+ 
